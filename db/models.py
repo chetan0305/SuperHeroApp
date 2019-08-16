6cdd7068c8d9ac3130e3c5b96e81587ccccded97
Chetan Pawar <chetan.pawar@kpn.com>
2019-08-16 14:20:01 +0200

    SuperHero App, First commit

---
+++ db/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class SuperHero(models.Model):
+    name = models.CharField(max_length=30)
+    biography = models.TextField()
+    publisher = models.ManyToManyField(Publisher)
+    alter_ego = models.CharField(max_length=30)
+    superpowers = models.TextField()
+    team_affiliation = models.ManyToManyField(Team)
+
+    def __str__(self):
+        return self.name
